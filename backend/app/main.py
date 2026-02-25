e31f7626ddb7e812c8e1524825b332cfef20e19f
Bilal Sadiq <bnaq860@gmail.com>
2026-02-25 16:59:26 +0530

    add Gemini LLM and MongoDB client

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.services.gemini_service import generate_response
+from app.config import client
 
 app = FastAPI()
 
+
+
+@app.get("/test")
+def test():
+    return generate_response("Hello, how are you?")
 
 @app.get("/")
 def health_check():
